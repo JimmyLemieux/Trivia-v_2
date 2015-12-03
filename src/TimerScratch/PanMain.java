24295ec563b2cd819b4799a0537724ac1e320b81
lemij7026 <lemij7026@FHC-IC0026892.wrdsb.ca>
2015-12-03 09:33:40 -0500

    two scratches

--- src/TimerScratch/PanMain.java
+++ src/TimerScratch/PanMain.java
@@ -4,10 +4,38 @@
  */
 package TimerScratch;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author lemij7026
  */
-public class PanMain {
+public class PanMain extends JPanel implements ActionListener {
+
+    JLabel label;
+    Timer timer = new Timer();
     
+
+    public PanMain() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int time = 0;
+            @Override
+            public void run() {
+                time++;
+                System.out.println(time);
+            }
+        }, 0, 1000);
+      
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
