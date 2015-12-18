79531a6cfddb5b1211d3c672c0e91b35a308882d
lemij7026 <lemij7026@FHC-IC0026892.wrdsb.ca>
2015-12-18 08:56:41 -0500

    SCRATCHES

--- src/JSONImages/Main.java
+++ src/JSONImages/Main.java
@@ -15,7 +15,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, Exception {
         FraMain main = new FraMain();
     }
 }
